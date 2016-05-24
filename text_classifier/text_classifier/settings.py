e47b0b0c5fb736f684b7c97cd2389574946fd79d
Scott Robertson <the.scott.rob@gmail.com>
2016-05-24 11:06:11 -0400

    the basics update .env FOR EVERYONEgit statusgit status

--- text_classifier/text_classifier/settings.py
+++ text_classifier/text_classifier/settings.py
@@ -35,7 +35,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 )
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
+    'PAGE_SIZE': 20
+}
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
