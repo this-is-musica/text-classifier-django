d2ba1c1fc521d411cc717118d33b960ca649f153
Scott Robertson <the.scott.rob@gmail.com>
2016-05-24 11:39:43 -0400

    now it has the app connected

--- text_classifier/text_classifier/settings.py
+++ text_classifier/text_classifier/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
+    'money_maker.apps.MoneyMakerConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
