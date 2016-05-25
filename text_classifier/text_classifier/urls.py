c04c3baf98ac885df12fc8acc1080ef361f42ee5
Scott Robertson <the.scott.rob@gmail.com>
2016-05-25 11:59:29 -0400

    refactoring vaporized positronic array

--- text_classifier/text_classifier/urls.py
+++ text_classifier/text_classifier/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from money_maker import views
+
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'text_classifier.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', views.index),
     url(r'^admin/', include(admin.site.urls)),
 ]
