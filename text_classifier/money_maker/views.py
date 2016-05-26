975e6c663d7a804d2cac7becfaaf40fc0cff2d27
Scott Robertson <the.scott.rob@gmail.com>
2016-05-26 11:59:37 -0400

    remember '.htmls' deploying to heroku v11

--- text_classifier/money_maker/views.py
+++ text_classifier/money_maker/views.py
@@ -5,16 +5,6 @@ from . import analysis
 
 def index(request):
     if request.method == "POST":
-        return display_genre(request,analysis.classify_lyrics(request.POST.get('name','')))
+        context = {"genre": analysis.classify_lyrics(request.POST.get('name',''))}
+        return render(request, 'money_maker/index.html', context)
     return render(request, 'money_maker/index.html')
-
-
-def test(request):
-    if request.method == "POST":
-        return HttpResponse(analysis.classify_lyrics(request.POST.get('lyrics','')))
-    else:
-        return render(request, 'money_maker/lyrics_in.html')
-
-def display_genre(request, genre):
-    context = {"genre": genre}
-    return render(request, 'money_maker/index', context)
